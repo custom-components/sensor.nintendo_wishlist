ac8aff80f834dd92d5a1cb725b89ee1cbc050aa3
Aaron Godfrey <me@aarongodfrey.dev>
2020-04-07 18:49:37 -0700

    Add binary sensor

---
+++ custom_components/nintendo_wishlist/binary_sensor.py
@@ -0,0 +1,17 @@
+import logging
+
+from homeassistant import core
+
+from .const import DOMAIN
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(
+    hass: core.HomeAssistant, config, async_add_entities, discovery_info=None
+):
+    """Setup the sensor platform."""
+    await hass.data[DOMAIN]["sensor_manager"].async_register_component(
+        "binary_sensor", async_add_entities
+    )
